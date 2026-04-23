4536e87c81d7cf55c7caa63bed88caae0c264bac
rommel dalisay <rommelmdalisay@gmail.com>
2026-04-23 12:50:59 +0800

    query

---
+++ sql.sql
@@ -0,0 +1,29 @@
+SELECT *
+FROM customer
+
+SELECT fname, lname, addressline, phone
+FROM customer
+
+SELECT town, lname
+FROM customer
+
+SELECT description, sell_price
+FROM item
+
+FROM orderinfo
+SELECT orderinfo_id, date_placed
+
+SELECT  DISTINCT town, lname 
+FROM customer
+
+SELECT description, sell_price
+FROM item
+ORDER BY description
+
+SELECT description, sell_price
+FROM item
+ORDER BY description DESC
+
+SELECT description, sell_price
+FROM item
+ORDER BY sell_price DESC
